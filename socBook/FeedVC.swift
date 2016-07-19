0fcc2461da635245f3df618e68a7599ce5d3e2e4
Alan Glasby <alan@glasby.me.uk>
2016-07-19 09:46:07 +0100

    Added post cell to UI

---
+++ socBook/FeedVC.swift
@@ -0,0 +1,35 @@
+//
+//  FeedVC.swift
+//  socBook
+//
+//  Created by Alan Glasby on 19/07/2016.
+//  Copyright © 2016 Alan Glasby. All rights reserved.
+//
+
+import UIKit
+
+class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.delegate = self
+        tableView.dataSource = self
+
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCellWithIdentifier("PostCell") as! PostCell
+    }
+
+}
---
+++ socBook/PostCell.swift
@@ -0,0 +1,35 @@
+//
+//  PostCell.swift
+//  socBook
+//
+//  Created by Alan Glasby on 19/07/2016.
+//  Copyright © 2016 Alan Glasby. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var profileImg: UIImageView!
+        @IBOutlet weak var showcaseImg: UIImageView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+
+    override func drawRect(rect: CGRect) {
+        profileImg.layer.cornerRadius = profileImg.frame.size.width / 2
+        profileImg.clipsToBounds = true
+
+        showcaseImg.clipsToBounds = true
+    }
+
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
